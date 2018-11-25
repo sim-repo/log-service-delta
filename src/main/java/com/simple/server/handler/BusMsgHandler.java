1c9ca6c17cffa968d930994562ac044aa7d3b031
ivivanov <ivivanov@msk10websvc4.simple.ru>
2018-11-25 22:07:36 +0300

    fix misc issues

--- src/main/java/com/simple/server/handler/BusMsgHandler.java
+++ src/main/java/com/simple/server/handler/BusMsgHandler.java
@@ -1,12 +1,21 @@
 package com.simple.server.handler;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Service;
 
 @Service("busMsgHandler")
 public class BusMsgHandler extends AbstractMsgHandler{
 	
 	
-	public void handleBusJsonMsg(String json) throws Exception {						
+	public void handleBusJsonMsg(String json) throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+		Date date = new Date();	
+		System.out.println("log: handle " + dateFormat.format(date));	
+	
 		getAppConfig().getDirtyBusJsonQueue().put(json);
 	}
 
